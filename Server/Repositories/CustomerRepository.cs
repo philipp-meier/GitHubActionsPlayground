3ede244d6de4bd02a50ed2ace959a63395b221be
Philipp Meier <philipp.meier@gmx.at>
2023-03-23 21:13:17 +0100

    Added customer

--- Server/Repositories/CustomerRepository.cs
+++ Server/Repositories/CustomerRepository.cs
@@ -9,7 +9,8 @@ public class CustomerRepository {
         _customers = new List<Customer> {
             new Customer { Id = Guid.Parse("61f57488-aa3d-41ff-b844-15824771d773"), FullName = "Customer 1" },
             new Customer { Id = Guid.Parse("391b273f-07a3-4d10-a0c1-be78502e811d"), FullName = "Customer 2" },
-            new Customer { Id = Guid.Parse("ddde6181-5349-47ba-b797-6411fe02d255"), FullName = "Customer 3" }
+            new Customer { Id = Guid.Parse("ddde6181-5349-47ba-b797-6411fe02d255"), FullName = "Customer 3" },
+            new Customer { Id = Guid.Parse("d2de6181-3349-47ba-b797-6411f102d255"), FullName = "Customer 4" }
         };
     }
 
